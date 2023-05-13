92d264727f869226bb09838b27f7934769d4792b
krisbobo <kiyongbohbo@gmail.com>
2023-05-13 22:12:25 +0100

    Create 3 more tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name varchar,
@@ -29,3 +28,21 @@ ALTER TABLE animals
   DROP COLUMN species;
 ALTER TABLE animals ADD species_id int REFERENCES species(id);
 ALTER TABLE animals ADD owner_id int REFERENCES owners(id);
+
+CREATE TABLE vets(
+    id SERIAL PRIMARY KEY,
+    name varchar,
+    age int,
+    date_of_graduation date
+)
+
+CREATE TABLE specializations (
+  vet_id int REFERENCES vets(id),
+  species_id int REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+  animal_id int REFERENCES animals(id),
+  date_of_visit date,
+  vet_id int REFERENCES vets(id)
+);
