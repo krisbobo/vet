302218e43d47eceac3fe2c6164d38c0837fb073f
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-15 11:02:25 +0200

    Confirm the query plan for all

--- queries.sql
+++ queries.sql
@@ -138,3 +138,7 @@ SELECT s.name, COUNT(*) as total_visits FROM vets ve
   LIMIT 1;
 
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
